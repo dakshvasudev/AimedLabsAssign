ce18639a8cc703d2559441b5bd6ae9df6bfa4dc2
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2023-01-16 17:30:09 +0530

    assignment completed

---
+++ android/app/src/main/kotlin/com/example/aimed_labs_internship_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aimed_labs_internship_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              padding: const EdgeInsets.all(8.0),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 130,
+                    width: double.infinity,
+                    color: Colors.red.shade100,
+                  ),
+                  Positioned(
+                    top: 25,
+                    left: 16,
+                    right: 250,
+                    child: Container(
+                      color: Colors.grey,
+                      height: 30,
+                      width: 300,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 25,
+                    left: 16,
+                    right: 16,
+                    child: Container(
+                      color: Colors.green.shade100,
+                      height: 30,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Positioned(
+                    top: 17,
+                    left: 8,
+                    right: 8,
+                    child: Container(
+                      height: 130,
+                      width: 600,
+                      color: Colors.red.shade100,
+                    ),
+                  ),
+                  Positioned(
+                    left: 150,
+                    right: 150,
+                    child: Container(
+                      color: Colors.grey,
+                      height: 30,
+                      width: 300,
+                    ),
+                  ),
+                  Positioned(
+                    top: 100,
+                    left: 16,
+                    right: 16,
+                    child: Container(
+                      color: Colors.green.shade100,
+                      height: 30,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
